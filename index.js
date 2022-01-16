f9c6e62f4b4b74ad5084e4f72621be4d27a915f9
Katarina <katarina.floyd1@gmail.com>
2022-01-16 15:26:54 +0100

    Comment out previous code

--- index.js
+++ index.js
@@ -1,73 +1,75 @@
 // let
 
-let number = 1; // defines a variable name
-let number2 = 2; //  
-console.log(number);
-number = 3; // 'let' lets you reassign variables (change the value of this variable name)
-console.log(number);
+// let number = 1; // defines a variable name
+// let number2 = 2; //  
+// console.log(number);
+// number = 3; // 'let' lets you reassign variables (change the value of this variable name)
+// console.log(number);
 
 // const
 
-const numm = 1; // defines a variable name
-let numm2 = 2; //  
-console.log(numm);
-// numm = 3; // there is an error, because you cannot reassign a const value
-console.log(numm);
+// const numm = 1; // defines a variable name
+// let numm2 = 2; //  
+// console.log(numm);
+// // numm = 3; // there is an error, because you cannot reassign a const value
+// console.log(numm);
 
 // Make default 'const' unless you are planning to change the variable
 
 // camelCase
 
-const numOfPeople = 1; // camelCase
-const num2 = 2; 
+// const numOfPeople = 1; // camelCase
+// const num2 = 2; 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // 5.3 Numbers, Booleans, Conditionals
 
-const myMoney = 300;
-const myFriendsMoney = 200 + 20; // a number in JS is called a 'primitive data type'
-console.log(myMoney + myFriendsMoney + myMoney / myMoney);
-// use Order of Operations (PEMDAS) when calculating inside the parentheses
-const totalMoney = myMoney + myFriendsMoney; // instead of just going in the console, this output is now saved
-console.log(totalMoney);
+// const myMoney = 300;
+// const myFriendsMoney = 200 + 20; // a number in JS is called a 'primitive data type'
+// console.log(myMoney + myFriendsMoney + myMoney / myMoney);
+// // use Order of Operations (PEMDAS) when calculating inside the parentheses
+// const totalMoney = myMoney + myFriendsMoney; // instead of just going in the console, this output is now saved
+// console.log(totalMoney);
 
-// remainder (called 'modulo' in programming)
+// // remainder (called 'modulo' in programming)
 
-const change = totalMoney % 100; // '%' is modulo, which will calculate the remainder when dividing by 100
-console.log(change);
+// const change = totalMoney % 100; // '%' is modulo, which will calculate the remainder when dividing by 100
+// console.log(change);
 
-// booleans
+// // booleans
 
-const iHaveMoreMoney = myMoney > myFriendsMoney; // the result of this expression is either true or false
-// can also use >= or >=, or == (equivalence), but you should always use === for JS to assert equality
-console.log(iHaveMoreMoney);
+// const iHaveMoreMoney = myMoney > myFriendsMoney; // the result of this expression is either true or false
+// // can also use >= or >=, or == (equivalence), but you should always use === for JS to assert equality
+// console.log(iHaveMoreMoney);
 
-// if-statement
+// // if-statement
 
-if (iHaveMoreMoney) {
-  console.log("I'm Rich");
-}
+// if (iHaveMoreMoney) {
+//   console.log("I'm Rich");
+// }
 
-if (myMoney > myFriendsMoney) {
-  console.log("I'm Rich");
-}
+// if (myMoney > myFriendsMoney) {
+//   console.log("I'm Rich");
+// }
 
-// less code isn't necessarily better, the clearest to read code is better
+// // less code isn't necessarily better, the clearest to read code is better
 
-if (iHaveMoreMoney) {
-  console.log("I'm Rich");
-} else {
-  console.log("I'm Poor");
-}
+// if (iHaveMoreMoney) {
+//   console.log("I'm Rich");
+// } else {
+//   console.log("I'm Poor");
+// }
 
-if (iHaveMoreMoney) {
-  console.log("I'm Rich");
-} else if (myMoney > 200) {
-  console.log("I'm doing okay"); // you can use as many 'else if' statements as you want
-} else {
-  console.log("I'm poor")
-}
+// if (iHaveMoreMoney) {
+//   console.log("I'm Rich");
+// } else if (myMoney > 200) {
+//   console.log("I'm doing okay"); // you can use as many 'else if' statements as you want
+// } else {
+//   console.log("I'm poor")
+// }
+
+
 
 
 
