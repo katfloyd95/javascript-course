83ccd4710c56b14eb39bef6e940a8f667cc9ee52
Katarina <katarina.floyd1@gmail.com>
2022-01-16 15:22:51 +0100

    Numbers, Booleans, & Conditionals

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ console.log(number);
 const numm = 1; // defines a variable name
 let numm2 = 2; //  
 console.log(numm);
-numm = 3; // there is an error, because you cannot reassign a const value
+// numm = 3; // there is an error, because you cannot reassign a const value
 console.log(numm);
 
 // Make default 'const' unless you are planning to change the variable
@@ -20,5 +20,56 @@ console.log(numm);
 
 const numOfPeople = 1; // camelCase
 const num2 = 2; 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// 5.3 Numbers, Booleans, Conditionals
+
+const myMoney = 300;
+const myFriendsMoney = 200 + 20; // a number in JS is called a 'primitive data type'
+console.log(myMoney + myFriendsMoney + myMoney / myMoney);
+// use Order of Operations (PEMDAS) when calculating inside the parentheses
+const totalMoney = myMoney + myFriendsMoney; // instead of just going in the console, this output is now saved
+console.log(totalMoney);
+
+// remainder (called 'modulo' in programming)
+
+const change = totalMoney % 100; // '%' is modulo, which will calculate the remainder when dividing by 100
+console.log(change);
+
+// booleans
+
+const iHaveMoreMoney = myMoney > myFriendsMoney; // the result of this expression is either true or false
+// can also use >= or >=, or == (equivalence), but you should always use === for JS to assert equality
+console.log(iHaveMoreMoney);
+
+// if-statement
+
+if (iHaveMoreMoney) {
+  console.log("I'm Rich");
+}
+
+if (myMoney > myFriendsMoney) {
+  console.log("I'm Rich");
+}
+
+// less code isn't necessarily better, the clearest to read code is better
+
+if (iHaveMoreMoney) {
+  console.log("I'm Rich");
+} else {
+  console.log("I'm Poor");
+}
+
+if (iHaveMoreMoney) {
+  console.log("I'm Rich");
+} else if (myMoney > 200) {
+  console.log("I'm doing okay"); // you can use as many 'else if' statements as you want
+} else {
+  console.log("I'm poor")
+}
+
+
+
 
 
