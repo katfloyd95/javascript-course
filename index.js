d254a0facb5ff7bca0b698a53266a7177a106dc1
Katarina <katarina.floyd1@gmail.com>
2022-01-16 14:44:38 +0100

    Variables

---
+++ index.js
@@ -0,0 +1,24 @@
+// let
+
+let number = 1; // defines a variable name
+let number2 = 2; //  
+console.log(number);
+number = 3; // 'let' lets you reassign variables (change the value of this variable name)
+console.log(number);
+
+// const
+
+const numm = 1; // defines a variable name
+let numm2 = 2; //  
+console.log(numm);
+numm = 3; // there is an error, because you cannot reassign a const value
+console.log(numm);
+
+// Make default 'const' unless you are planning to change the variable
+
+// camelCase
+
+const numOfPeople = 1; // camelCase
+const num2 = 2; 
+
+
